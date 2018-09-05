a871c1d479c52664814cdfe67153fc5e2afc8735
chinzou <chine.zoheir@gmail.com>
2018-09-05 12:33:40 +0100

    INFRA-159: delete checking openmrs using curl

--- resources/stop_bahmni.sh
+++ resources/stop_bahmni.sh
@@ -4,7 +4,7 @@ host_is_up()
 	if (curl -L -k -I -s localhost/openmrs | grep -q 'HTTP/1.1 200 OK')
 	then
 		return 1
-	else 
+	else
 		return 0
 	fi
 }
@@ -14,11 +14,8 @@ if service --status-all | grep -Fq 'postgresql-9.2'; then
 fi
 bahmni -ilocal start
 
-while ( host_is_up = 0 )
-do
 echo "waiting openmrs startup..."
-sleep 10
-done
+sleep 180
 
 bahmni -ilocal stop
 service mysqld stop
