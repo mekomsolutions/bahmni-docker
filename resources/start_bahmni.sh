5f0f3bc34f3dee3b9b20999713efe5b12fb75643
chine zoheir <chine.zoheir@gmail.com>
2018-11-20 20:25:25 +0800

    INFRA-173: Delete OpenERP PID file at startup (#4)

--- resources/start_bahmni.sh
+++ resources/start_bahmni.sh
@@ -1,3 +1,4 @@
+[ -e /var/run/openerp/openerp-server.pid ] && rm -f /var/run/openerp/openerp-server.pid
 service mysqld start
 if service --status-all | grep -Fq 'postgresql-9.2'; then    
   service postgresql-9.2 start
