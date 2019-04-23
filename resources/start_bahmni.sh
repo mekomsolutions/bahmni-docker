36d29afc5ca25f8b9ebf830cace0eb83d297ad59
fabien <fabien@mekomsolutions.com>
2019-04-23 10:30:46 +0200

    BAH-783: Delete files from previous project

--- resources/start_bahmni.sh
+++
@@ -1,6 +0,0 @@
-[ -e /var/run/openerp/openerp-server.pid ] && rm -f /var/run/openerp/openerp-server.pid
-service mysqld start
-if service --status-all | grep -Fq 'postgresql-9.2'; then    
-  service postgresql-9.2 start
-fi
-bahmni -ilocal start
