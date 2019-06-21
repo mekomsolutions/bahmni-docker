e7f5c0ff207bcfb871fb76afe1c4d4577ab08813
chinzou <chine.zoheir@gmail.com>
2019-06-21 11:18:30 +0800

    INFRA-213: Bahmni docker to setup bahmni 0.92 image

---
+++ resources/install_bahmni.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A bash script to install bahmni 0.92 in a docker image
+
+bahmni -ilocal install 
+bahmni -ilocal stop
--- resources/start_bahmni.sh
+++ resources/start_bahmni.sh
@@ -1,6 +1,2 @@
-[ -e /var/run/openerp/openerp-server.pid ] && rm -f /var/run/openerp/openerp-server.pid
-service mysqld start
-if service --status-all | grep -Fq 'postgresql-9.2'; then    
-  service postgresql-9.2 start
-fi
+#!/bin/bash
 bahmni -ilocal start
