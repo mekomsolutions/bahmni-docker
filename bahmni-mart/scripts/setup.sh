5f9716853b67af7888d455b1594415863ede7dc5
bhiravabhatla <36130219+bhiravabhatla@users.noreply.github.com>
2019-11-06 00:29:41 +0530

    BAH-924: Add bahmni-mart components (#21)

---
+++ bahmni-mart/scripts/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+setup_properties(){
+    envsubst < /tmp/application.properties > /opt/bahmni-mart/properties/application.properties
+    chmod 644 /opt/bahmni-mart/properties/application.properties /opt/bahmni-mart/conf/liquibase.xml
+}
+
+setup_cronjob() {
+   echo "$CRON_TIME  java -jar /opt/bahmni-mart/lib/bahmni-mart.jar --spring.config.location=\"/opt/bahmni-mart/properties/application.properties\" > /proc/1/fd/1 2>/proc/1/fd/2" | crontab - 
+}
+
+setup_properties
+echo "*********** PSQL DB Creation Starts   ******************"
+sh /etc/scripts/createdbuser.sh
+echo "*********** PSQL DB Creation Ends   ******************"
+setup_cronjob
+echo "********** Post Install Script Completed ***************"
+crond -f -d 8
