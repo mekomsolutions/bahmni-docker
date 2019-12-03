0dcb36e0a8fac784f380a4ed78df98dded0dfabb
chinzou <chine.zoheir@gmail.com>
2019-12-04 00:35:29 +0800

    BAH-783: Rename service names

--- bahmni-mart/scripts/createdbuser.sh
+++ bahmni-mart/scripts/createdbuser.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
-until PGPASSWORD=$POSTGRES_PASSWORD psql -hpostgres-mart -Upostgres postgres &>/dev/null
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h$DB_MART -Upostgres postgres &>/dev/null
 do
   echo "Waiting for PostgreSQL..."
   sleep 1
 done
-if  PGPASSWORD=$POSTGRES_PASSWORD psql -hpostgres-mart -Upostgres -lqt | cut -d \| -f 1 | grep -qw analytics; then
+if  PGPASSWORD=$POSTGRES_PASSWORD psql -h$DB_MART -Upostgres -lqt | cut -d \| -f 1 | grep -qw analytics; then
    echo "Analytics Database already exists!!"
 else
-    PGPASSWORD=$POSTGRES_PASSWORD psql -hpostgres-mart -Upostgres <<-EOSQL
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h$DB_MART -Upostgres <<-EOSQL
         CREATE USER analytics WITH PASSWORD '$ANALYTICS_PASSWORD';
         CREATE DATABASE analytics OWNER analytics;
 EOSQL
 fi
-if  PGPASSWORD=$POSTGRES_PASSWORD psql -hpostgres-mart -Upostgres -lqt | cut -d \| -f 1 | grep -qw metabase; then
+if  PGPASSWORD=$POSTGRES_PASSWORD psql -h$DB_MART -Upostgres -lqt | cut -d \| -f 1 | grep -qw metabase; then
    echo "Metabase Database already exists!!"
 else
-    PGPASSWORD=$POSTGRES_PASSWORD psql -hpostgres-mart -Upostgres <<-EOSQL
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h$DB_MART -Upostgres <<-EOSQL
         CREATE USER metabase WITH PASSWORD '$MB_DB_PASS';
         CREATE DATABASE metabase OWNER metabase;
 EOSQL
