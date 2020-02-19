6a262dc37d9beb1cb3ef0ecb536373de4b659481
chinzou <chine.zoheir@gmail.com>
2020-02-19 21:48:04 +0800

    BAH-849: Update odoo-connect service

--- odoo-connect/resources/run-liquibase.sh
+++ odoo-connect/resources/run-liquibase.sh
@@ -16,10 +16,10 @@ ODOO_DB_USERNAME=${ODOO_DB_USERNAME:-odoo}
 ODOO_DB_PASSWORD=${ODOO_DB_PASSWORD:-odoo}
 
 CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
-LIQUIBASE_JAR="$WEBAPP_PATH/bahmni-erp-connect/WEB-INF/lib/liquibase-core-2.0.3.jar"
+LIQUIBASE_JAR="/etc/liquibase-core-2.0.3.jar"
 DRIVER="org.postgresql.Driver"
 CREDS="--url=jdbc:postgresql://${ODOO_DB_SERVER}:5432/${ODOO_DB} --username=${ODOO_DB_USERNAME} --password=${ODOO_DB_PASSWORD}"
-CLASSPATH="${WEBAPP_PATH}/bahmni-erp-connect.war"
-CHANGE_LOG_FILE="${WEBAPP_PATH}/bahmni-erp-connect/WEB-INF/classes/sql/db_migrations.xml"
+CLASSPATH="/etc/bahmni-erp-connect.war"
+CHANGE_LOG_FILE="/etc/bahmni-erp-connect/db_migration.xml"
 
 java ${CHANGE_LOG_TABLE} -jar ${LIQUIBASE_JAR} --driver=${DRIVER} --classpath=${CLASSPATH} --changeLogFile=${CHANGE_LOG_FILE} ${CREDS} update
--- odoo-connect/startup.sh
+++ odoo-connect/startup.sh
@@ -1,23 +1,14 @@
 #!/bin/bash -eux
 
+. /usr/local/tomcat/bin/setenv.sh
+
 DEBUG=${DEBUG:-false}
 
 # wait for postgresql to initialise
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${ODOO_DB_SERVER}:5432
 
-
-if [ $DEBUG ]; then
-    export JPDA_ADDRESS="1044"
-    export JPDA_TRANSPORT=dt_socket
-fi
-
-
-# start tomcat
-/usr/local/tomcat/bin/catalina.sh jpda run &
-
 # run liquibase migrations
-sleep 3
 /etc/bahmni-erp-connect/run-liquibase.sh
 
-# bring tomcat process to foreground again
-wait ${!}
+# start tomcat
+/usr/local/tomcat/bin/catalina.sh jpda run
