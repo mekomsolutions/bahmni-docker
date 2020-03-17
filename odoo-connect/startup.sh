c814a820e8ec42a697e318a0a7d7f2ffb5ece77e
Zoheir CHINE <chine.zoheir@gmail.com>
2020-03-17 19:08:20 +0800

    BAH-849: Enable DEBUG option in odoo-connect (#27)

--- odoo-connect/startup.sh
+++ odoo-connect/startup.sh
@@ -3,6 +3,8 @@
 . /usr/local/tomcat/bin/setenv.sh
 
 DEBUG=${DEBUG:-false}
+DEBUG_PORT=${DEBUG_PORT:-8000}
+catalina_params=()
 
 # wait for postgresql to initialise
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${ODOO_DB_SERVER}:5432
@@ -11,5 +13,12 @@ DEBUG=${DEBUG:-false}
 sleep 3
 /etc/bahmni-erp-connect/run-liquibase.sh
 
+if [ $DEBUG == true ]; then
+    export JPDA_ADDRESS=$DEBUG_PORT
+    export JPDA_TRANSPORT=dt_socket
+    catalina_params+=(jpda)
+fi
+catalina_params+=(run)
+
 # start tomcat
-/usr/local/tomcat/bin/catalina.sh jpda run
+/usr/local/tomcat/bin/catalina.sh "${catalina_params[@]}"
