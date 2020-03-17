4fb11c8e0d3c6d1c10907f2ac9c7f768327085ee
chinzou <chine.zoheir@gmail.com>
2020-03-17 15:59:04 +0800

    BAH-849: Set default debug port to 8000

--- odoo-connect/startup.sh
+++ odoo-connect/startup.sh
@@ -3,6 +3,8 @@
 . /usr/local/tomcat/bin/setenv.sh
 
 DEBUG=${DEBUG:-false}
+DEBUG_PORT=${DEBUG_PORT:-8000}
+catalina_params=()
 
 # wait for postgresql to initialise
 /usr/local/tomcat/wait-for-it.sh --timeout=3600 ${ODOO_DB_SERVER}:5432
@@ -12,12 +14,11 @@ sleep 3
 /etc/bahmni-erp-connect/run-liquibase.sh
 
 if [ $DEBUG == true ]; then
-    export JPDA_ADDRESS="1044"
+    export JPDA_ADDRESS=$DEBUG_PORT
     export JPDA_TRANSPORT=dt_socket
-
-    # start tomcat with debug mode enabled
-    /usr/local/tomcat/bin/catalina.sh jpda run
-else
-    # start tomcat
-    /usr/local/tomcat/bin/catalina.sh run
+    catalina_params+=(jpda)
 fi
+catalina_params+=(run)
+
+# start tomcat
+/usr/local/tomcat/bin/catalina.sh "${catalina_params[@]}"
