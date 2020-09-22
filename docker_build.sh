54d15eb157f0b153615b21b8e12140fd5e92b107
Zoheir CHINE <chine.zoheir@gmail.com>
2020-09-22 18:09:52 +0800

    INFRA-243: Build and push images with Travis (#40)

---
+++ docker_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo fine
+Services=appointments,bahmni-mart,bahmni-reports,bahmniapps,implementer-interface,metabase,odoo,odoo-connect,openmrs
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+for service in ${Services//,/ }
+do
+    echo "Building $service $TRAVIS_CPU_ARCH image"
+    docker build ${service}/ -t $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_$TRAVIS_CPU_ARCH
+    echo "Pushing $service $TRAVIS_CPU_ARCH image"
+    docker push $DOCKER_USERNAME/${service}:$(git rev-parse --short HEAD)_$TRAVIS_CPU_ARCH
+done
