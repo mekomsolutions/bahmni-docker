beeb7f4fdd7482aaea5deec7e64a3a3b635ce211
rbuisson <romain@mekomsolutions.com>
2021-02-10 20:36:43 +0100

    C2C-30: Use Metabase Enterprise image

--- metabase/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-. /usr/local/tomcat/bin/setenv.sh
-
-# wait for postgresql to initialise
-/usr/local/tomcat/wait-for-it.sh --timeout=3600 ${MB_DB_HOST}:5432
-
-# start tomcat
-java -jar /usr/local/tomcat/webapps/metabase.jar
