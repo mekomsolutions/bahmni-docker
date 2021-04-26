7c573bec7c956160b01a3a55e9888b3bdd5a90de
rbuisson <romain@mekomsolutions.com>
2021-04-26 15:17:52 +0200

    Update 34

--- build/main.tf
+++ build/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "arm64_builder" {
     "SSH"
   ]
   tags = {
-    Name   = "[CI] [Ubuntu] [ARM64 Docker Builder] [Terraform]",
+    Name   = "[GitHub Actions] [Ubuntu] [ARM64 Docker Builder] [Terraform]",
     Type   = "test",
     Owner  = "${data.aws_caller_identity.current.user_id}",
     Groups = "arm64_builder, terraform",
@@ -44,7 +44,7 @@ resource "aws_instance" "amd64_builder" {
     "SSH"
   ]
   tags = {
-    Name   = "[CI] [Ubuntu] [AMD64 Docker Builder] [Terraform]",
+    Name   = "[GitHub Actions] [Ubuntu] [AMD64 Docker Builder] [Terraform]",
     Type   = "test",
     Owner  = "${data.aws_caller_identity.current.user_id}",
     Groups = "amd64_builder, terraform",
