290ec17f48eea9ea3de92a042fdc3288e7cf61d9
rbuisson <romain@mekomsolutions.com>
2021-04-26 14:43:55 +0200

    Update 28

--- build/main.tf
+++ build/main.tf
@@ -16,7 +16,7 @@ resource "tls_private_key" "host-ecdsa" {
   algorithm = "ECDSA"
 }
 
-resource "aws_instance" "arm_builder" {
+resource "aws_instance" "arm64_builder" {
   ami           = "ami-0f4fb321056ea2a14"
   instance_type = "a1.xlarge"
   security_groups = [
@@ -24,31 +24,48 @@ resource "aws_instance" "arm_builder" {
     "SSH"
   ]
   tags = {
-    Name   = "[CI] [Ubuntu] [ARM Docker Builder] [Terraform]",
+    Name   = "[CI] [Ubuntu] [ARM64 Docker Builder] [Terraform]",
     Type   = "test",
     Owner  = "${data.aws_caller_identity.current.user_id}",
-    Groups = "arm_builder, terraform",
+    Groups = "arm64_builder, terraform",
     User   = "centos"
   }
   root_block_device {
     volume_type           = "gp2"
     delete_on_termination = "true"
   }
-  user_data = <<EOF
-#!/bin/bash
-umask 077
-echo '${tls_private_key.host-ecdsa.private_key_pem}' >/etc/ssh/ssh_host_ecdsa_key
-echo '${tls_private_key.host-rsa.private_key_pem}' >/etc/ssh/ssh_host_rsa_key
-# Remove unsupported keys (Terraform can't generate DSA and ED25519 keys)
-rm /etc/ssh/ssh_host_dsa_key
-rm /etc/ssh/ssh_host_ed25519_key
-service ssh restart
-EOF
-}
-output "arm_builder_stats" {
+
+output "arm64_builder_stats" {
   value = {
-    ip     = "${aws_instance.arm_builder.public_ip}",
-    groups = "${aws_instance.arm_builder.tags.Groups}"
-    user   = "${aws_instance.arm_builder.tags.User}"
+    ip     = "${aws_instance.arm64_builder.public_ip}",
+    groups = "${aws_instance.arm64_builder.tags.Groups}"
+    user   = "${aws_instance.arm64_builder.tags.User}"
+  }
+}
+
+resource "aws_instance" "amd64_builder" {
+  ami           = "ami-0f4fb321056ea2a14"
+  instance_type = "t2.xlarge"
+  security_groups = [
+    "Ping",
+    "SSH"
+  ]
+  tags = {
+    Name   = "[CI] [Ubuntu] [AMD64 Docker Builder] [Terraform]",
+    Type   = "test",
+    Owner  = "${data.aws_caller_identity.current.user_id}",
+    Groups = "amd64_builder, terraform",
+    User   = "centos"
+  }
+  root_block_device {
+    volume_type           = "gp2"
+    delete_on_termination = "true"
+  }
+
+output "amd64_builder_stats" {
+  value = {
+    ip     = "${aws_instance.amd64_builder.public_ip}",
+    groups = "${aws_instance.amd64_builder.tags.Groups}"
+    user   = "${aws_instance.amd64_builder.tags.User}"
   }
 }
