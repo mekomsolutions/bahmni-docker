c7ca621cc829994c28eb58224b678b2283175d12
Zoheir CHINE <chine.zoheir@gmail.com>
2021-11-16 16:57:36 +0300

    HSC-50: Add liquibase changes to disable products sync (#62)

--- odoo-connect/init-db.sh
+++ odoo-connect/init-db.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 # Extract liquibase files from WAR archive
@@ -9,6 +9,8 @@ mkdir -p ${MIGRATIONS_FILES_FOLDER}/
 cp /tmp/bahmni-erp-connect/WEB-INF/classes/sql/db_migrations.xml ${MIGRATIONS_FILES_FOLDER}/db_migrations.xml
 rm -rf /tmp/bahmni-erp-connect
 echo "Done"
+
+DISABLE_PRODUCT_SYNC=${DISABLE_PRODUCT_SYNC:-true}
 
 ODOO_DB=${ODOO_DB:-odoo}
 ODOO_DB_SERVER=${ODOO_DB_SERVER:-odoo-postgresql}
@@ -21,9 +23,14 @@ DRIVER="org.postgresql.Driver"
 CREDS="--url=jdbc:postgresql://${ODOO_DB_SERVER}:5432/${ODOO_DB} --username=${ODOO_DB_USERNAME} --password=${ODOO_DB_PASSWORD}"
 CLASSPATH="${CATALINA_HOME}/webapps/bahmni-erp-connect.war"
 CHANGE_LOG_FILE="/opt/migrations/db_migrations.xml"
+DISABLE_CONCEPT_SYNC_FILE="/opt/migrations/disable_product_sync.xml"
 
 if ! java ${CHANGE_LOG_TABLE} -jar ${LIQUIBASE_JAR} --driver=${DRIVER} --classpath=${CLASSPATH} --changeLogFile=${CHANGE_LOG_FILE} ${CREDS} update; then
     echo "Clear liquibase lock"
     PGPASSWORD=$ODOO_DB_PASSWORD psql -h $ODOO_DB_SERVER -U $ODOO_DB_USERNAME $ODOO_DB -c 'UPDATE liquibasechangeloglock SET locked=FALSE;'
     java ${CHANGE_LOG_TABLE} -jar ${LIQUIBASE_JAR} --driver=${DRIVER} --classpath=${CLASSPATH} --changeLogFile=${CHANGE_LOG_FILE} ${CREDS} update
+fi
+
+if [ $DISABLE_PRODUCT_SYNC == true ]; then
+    java ${CHANGE_LOG_TABLE} -jar ${LIQUIBASE_JAR} --driver=${DRIVER} --classpath=${CLASSPATH} --changeLogFile=${DISABLE_CONCEPT_SYNC_FILE} ${CREDS} update
 fi
